c1adcf96a00b71a717af6bf276f190c8d2a18919
yoavrotems <yoavrotems97@gmail.com>
2020-07-09 15:07:56 +0300

    Add tests from trimVersion

--- app_test.go
+++ app_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"reflect"
 	"testing"
 )
 
@@ -75,7 +76,9 @@ func Test_getDockerCisVersion(t *testing.T) {
 		{name: "Test for version 18.09", stringVersion: "18.09", want: "cis-1.2", wantErr: false},
 		{name: "Test for version 19.3.6", stringVersion: "19.3.6", want: "cis-1.2", wantErr: false},
 		{name: "Test for version 18.06", stringVersion: "18.06", want: "cis-1.1", wantErr: false},
+		{name: "Test for version 18.06.0-ce", stringVersion: "18.06.0-ce", want: "cis-1.1", wantErr: false},
 		{name: "Test for version 17.12", stringVersion: "17.12", want: "cis-1.1", wantErr: false},
+		{name: "Test for version 17.12-beta", stringVersion: "17.12-beta", want: "cis-1.1", wantErr: false},
 		{name: "Test for version 17.06", stringVersion: "17.06", want: "cis-1.1", wantErr: false},
 		{name: "Test for version 17.04", stringVersion: "17.04", want: "cis-1.0", wantErr: false},
 		{name: "Test for version 17.03", stringVersion: "17.03", want: "cis-1.0", wantErr: false},
@@ -93,6 +96,31 @@ func Test_getDockerCisVersion(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("getDockerCisVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_trimVersion(t *testing.T) {
+
+	tests := []struct {
+		name          string
+		stringVersion string
+		want          string
+	}{
+		{name: "Test remove beta", stringVersion: "18.9.3-beta", want: "18.9.3"},
+		{name: "Test remove ce", stringVersion: "17.6-ce", want: "17.6.0"},
+		{name: "Test remove metadata", stringVersion: "16.3+rc231", want: "16.3.0"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := trimVersion(tt.stringVersion)
+			if err != nil {
+				t.Errorf("trimVersion() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got.String(), tt.want) {
+				t.Errorf("trimVersion() = %v, want %v", got.String(), tt.want)
 			}
 		})
 	}

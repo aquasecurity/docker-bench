688e8ffa6ee2177b9461e0daddb1ac6441e3874a
yoavrotems <yoavrotems97@gmail.com>
2020-09-03 13:35:13 +0300

    add benchmarkVersion
    support both use of docker version and benchmark version input https://github.com/aquasecurity/docker-bench/issues/83

--- root.go
+++ root.go
@@ -30,6 +30,7 @@ var (
 	noRemediations bool
 
 	dockerVersion     string
+	benchmarkVersion  string
 	cfgDir            string
 	cfgFile           string
 	checkList         string
@@ -73,6 +74,7 @@ func init() {
 	RootCmd.PersistentFlags().BoolVar(&noSummary, "nosummary", false, "Disable printing of summary section")
 	RootCmd.PersistentFlags().BoolVar(&noRemediations, "noremediations", false, "Disable printing of remediations section")
 	RootCmd.Flags().StringVarP(&dockerVersion, "version", "", "", "Specify Docker version, automatically detected if unset")
+	RootCmd.Flags().StringVarP(&benchmarkVersion, "benchmark", "", "", "Manually specify CIS benchmark version. It would be an error to specify both --version and --benchmark flags")
 	RootCmd.Flags().StringVarP(&cfgDir, "config-dir", "D", "cfg", "directory to get benchmark definitions")
 	RootCmd.PersistentFlags().BoolVar(&jsonFmt, "json", false, "Prints the results as JSON")
 	RootCmd.PersistentFlags().BoolVar(&includeTestOutput, "include-test-output", false, "Prints the test's output")
